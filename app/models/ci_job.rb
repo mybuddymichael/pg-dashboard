4efff3de46173ff746c31a9ba1959a36cf07c235
Michael Hanson <michael.b.hanson@gmail.com>
2012-11-30 14:23:11 -0800

    Add beginning CiJob classes

---
+++ app/models/ci_job.rb
@@ -0,0 +1,18 @@
+class CiJob < ActiveResource::Base
+  self.site = "http://jenkins.progauge.us/"
+  self.user = ENV["jenkins_username"]
+  self.password = ENV["jenkins_api_token"]
+  self.collection_name = "job"
+
+  def self.element_path(name, prefix_options = {}, query_options = nil)
+    check_prefix_options(prefix_options)
+    prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+    "#{prefix(prefix_options)}#{collection_name}/#{URI.parser.escape name.to_s}/api/json#{query_string(query_options)}"
+  end
+
+  def self.collection_path(prefix_options = {}, query_options = nil)
+    check_prefix_options(prefix_options)
+    prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+    "#{prefix(prefix_options)}api/json#{query_string(query_options)}"
+  end
+end
---
+++ test/unit/ci_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CiJobTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
