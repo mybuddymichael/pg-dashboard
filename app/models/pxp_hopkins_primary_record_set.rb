99b6d30f71acf07eb7e30c0ebf0b6993282d6329
Michael Hanson <michael.b.hanson@gmail.com>
2012-11-30 11:58:48 -0800

    Remove the record set model classes

--- app/models/pxp_hopkins_primary_record_set.rb
+++
@@ -1,6 +0,0 @@
-class PxpHopkinsPrimaryRecordSet < ActiveRecord::Base
-  if Rails.env == "production"
-    establish_connection "client_icp_pxp_hopkins"
-    self.table_name = "sync_icp_record_set"
-  end
-end
--- test/unit/pxp_hopkins_primary_record_set_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PxpHopkinsPrimaryRecordSetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
