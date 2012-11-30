3fde60c741e15e3be99eba11ba2cf379a30c7b31
Aris Turner <aturner@proguage.us>
2012-11-30 14:21:58 -0800

    Added dashboard controller, set index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Dashboard::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  resources :dashboard
 
+
+  root :to => "dashboard#index"
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
