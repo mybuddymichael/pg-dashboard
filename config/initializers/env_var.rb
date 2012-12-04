6577e96f61e6e41eb2c5834593ba901f03a40f7f
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-04 13:36:15 -0800

    Load EnvVars before the app is initialized

--- config/initializers/env_var.rb
+++ config/initializers/env_var.rb
@@ -1,20 +1,22 @@
-module EnvVar
-  DATABASE_USERNAME = ENV["dashboard_database_username"]
-  DATABASE_PASSWORD = ENV["dashboard_database_password"]
-  DATABASE_HOSTNAME = ENV["dashboard_database_hostname"]
-  DATABASE_PORT = ENV["dashboard_database_port"]
+Rails.application.config.before_initialize do
+  module EnvVar
+    DATABASE_USERNAME = ENV["dashboard_database_username"]
+    DATABASE_PASSWORD = ENV["dashboard_database_password"]
+    DATABASE_HOSTNAME = ENV["dashboard_database_hostname"]
+    DATABASE_PORT = ENV["dashboard_database_port"]
 
-  CI_USERNAME = ENV["dashboard_ci_username"]
-  CI_PASSWORD = ENV["dashboard_ci_password"]
-  CI_HOSTNAME = ENV["dashboard_ci_hostname"]
+    CI_USERNAME = ENV["dashboard_ci_username"]
+    CI_PASSWORD = ENV["dashboard_ci_password"]
+    CI_HOSTNAME = ENV["dashboard_ci_hostname"]
 
-  ISSUE_TRACKER_USERNAME = ENV["dashboard_issue_tracker_username"]
-  ISSUE_TRACKER_PASSWORD = ENV["dashboard_issue_tracker_password"]
-  ISSUE_TRACKER_HOSTNAME = ENV["dashboard_issue_tracker_hostname"]
+    ISSUE_TRACKER_USERNAME = ENV["dashboard_issue_tracker_username"]
+    ISSUE_TRACKER_PASSWORD = ENV["dashboard_issue_tracker_password"]
+    ISSUE_TRACKER_HOSTNAME = ENV["dashboard_issue_tracker_hostname"]
 
-  self.constants.each do |constant|
-    if EnvVar.const_get(constant).nil?
-      raise TypeError, "EnvVar::#{constant} is nil! Define the relevant environment variable first. (See `config/initializers/env_var.rb` for necessary env vars.)"
+    self.constants.each do |constant|
+      if EnvVar.const_get(constant).nil?
+        raise TypeError, "EnvVar::#{constant} is nil! Define the relevant environment variable first. (See `config/initializers/env_var.rb` for necessary env vars.)"
+      end
     end
   end
 end
