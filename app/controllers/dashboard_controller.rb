07fdf9aadbfb12da5a580e57b759bb00d330655b
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-05 09:33:55 -0800

    Change some ivars to regular variables

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,15 +2,15 @@ class DashboardController < ApplicationController
 
   def index
     @title = "ICP"
-    @icp_connect_interval = (120)  # seconds
-    @now = Time.now.utc - (60*60*8)
+    icp_connect_interval = (120)  # seconds
+    now = Time.now.utc - (60*60*8)
     @all_icps = Icp.find(:all).collect do |icp|
-      @status = "blue"
-      if icp.last_connect_time < (@now - @icp_connect_interval)
+      status = "blue"
+      if icp.last_connect_time < (now - icp_connect_interval)
         @status = "orange"
-      elsif icp.last_sync_time < (@now - (60*icp.sync_interval))
+      elsif icp.last_sync_time < (now - (60*icp.sync_interval))
         @status = "orange"
-      elsif icp.last_parse_time < (@now - (2*60*icp.sync_interval))
+      elsif icp.last_parse_time < (now - (2*60*icp.sync_interval))
         @status = "orange"
       end
 
