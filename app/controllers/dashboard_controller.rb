3f64941cacf56962455138be58890f0ab86a41fc
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-05 14:25:51 -0800

    Change the timezone converter method name and add docs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,9 +11,9 @@ class DashboardController < ApplicationController
       status = :good
       messages = []
 
-      last_connect = convert_time(icp.last_connect_time, icp.timezone)
-      last_sync = convert_time(icp.last_sync_time, icp.timezone)
-      last_parse = convert_time(icp.last_parse_time, icp.timezone)
+      last_connect = convert_timezone_without_changing_time(icp.last_connect_time, icp.timezone)
+      last_sync = convert_timezone_without_changing_time(icp.last_sync_time, icp.timezone)
+      last_parse = convert_timezone_without_changing_time(icp.last_parse_time, icp.timezone)
 
       if last_connect < (now - ICP_CONNECTION_THRESHOLD_IN_SECONDS)
         status = :bad
@@ -50,7 +50,12 @@ class DashboardController < ApplicationController
 
   private
 
-  def convert_time(time, timezone)
+  # Internal: Convert an ActiveSupport::TimeWithZone object to the
+  # timezone provided in the "timezone" String, without changing the
+  # time itself.
+  #
+  # Returns a DateTime object.
+  def convert_timezone_without_changing_time(time, timezone)
     tz = TZInfo::Timezone.get(timezone).current_period.utc_total_offset
     tz = tz / 3600
     converted_time = time.to_datetime.change(offset: "#{tz}")
