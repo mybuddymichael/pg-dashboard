88dc24e2616fbf922c285d8c06f351d95d1e2a40
Aris Turner <bootstraps3412@gmail.com>
2012-12-04 16:37:53 -0800

    Collecting certain data from icp

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,23 @@ class DashboardController < ApplicationController
   def index
     @title = "ICP"
     @icp_connect_interval = (120)  # seconds
-    @icps = Icp.all
-    @now = Time.now.utc - (60*60*8) - (@icp_connect_interval)
+    @now = Time.now.utc - (60*60*8)
+    @all_icps = Icp.find(:all).collect do |icp|
+      @status = "blue"
+      if icp.last_connect_time < (@now - @icp_connect_interval)
+        @status = "orange"
+      elsif icp.last_sync_time < (@now - (60*icp.sync_interval))
+        @status = "orange"
+      elsif icp.last_parse_time < (@now - (2*60*icp.sync_interval))
+        @status = "orange"
+      end
+
+      {
+        "name" => icp.icp_name,
+        "status" => @status,
+      }
+    end
+    logger.info(@all_icps[0])
+    logger.info("------------------------------------------------")
   end
 end
