85bb9833cd4b81c8f3a830e5522a732d746527d2
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-06 15:56:48 -0800

    Add a CiBuild url reader

--- app/models/ci_build.rb
+++ app/models/ci_build.rb
@@ -8,12 +8,13 @@ class CiBuild
   #
   # Returns a CiBuild.
   def self.find_by_project_name_and_build_number(project_name, build_number)
-    url = BASE_URL + project_name + "/#{build_number}/" + API_SUFFIX
+    build_url = BASE_URL + project_name + "/#{build_number}/"
+    url = build_url + API_SUFFIX
 
     response = HTTParty.get(url, OPTIONS_HASH)
     build_hash = ActiveSupport::JSON.decode(response.body)
 
-    new(build_hash["number"], build_hash["result"])
+    new(build_hash["number"], build_hash["result"], build_url)
   end
 
   # Public: Returns a Fixnum of the build's number.
@@ -22,9 +23,13 @@ class CiBuild
   # Public: Returns a String of the build's result.
   attr_reader :result
 
-  def initialize(number, result)
+  # Public: Returns a String of the build's URL.
+  attr_reader :url
+
+  def initialize(number, result, url)
     @number = number
     @result = result
+    @url = url
   end
 
 end
