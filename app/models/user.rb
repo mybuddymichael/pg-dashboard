a9d6d4bcd22b4113f9496ecaf2df4c68a8c7874b
Aris Turner <bootstraps3412@gmail.com>
2012-12-06 16:06:03 -0800

    Add user authenticate_signin method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
     establish_connection "pdms_accounts"
     self.table_name = "user"
   end
+
+
+  def authenticate_signin (password)
+    @user = User.find_by_password(
+      Digest::SHA1.hexdigest(password + self.username))
+    if @user
+      return true
+    else
+      return false
+    end
+  end
 end
