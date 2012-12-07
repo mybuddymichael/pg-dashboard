4883eb9d8fb184d4c547456ae27c38f627661295
Aris Turner <bootstraps3412@gmail.com>
2012-12-06 16:18:52 -0800

    sign in form working correctly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_username(params[:session][:username].downcase)
+    if @user && @user.authenticate_signin(params[:session][:password])
+      redirect_to root_path
+    else
+      flash[:error] = "Invalid email/password combination!"
+      render 'new'
+    end
   end
 
   def destroy
