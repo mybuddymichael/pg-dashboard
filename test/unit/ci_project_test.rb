a7c0c3291f72a53e4011cee8669bc3f7bd0c1e08
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-10 10:41:47 -0800

    Add a test for CiProject#last_build

--- test/unit/ci_project_test.rb
+++ test/unit/ci_project_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class CiPojectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "last_build returns a CiBuild" do
+    assert_equal(CiProject.all.first.last_build.class, CiBuild)
+  end
 end
