8004efa3694ba4bb25426c13d9a3c5b32d668138
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-10 10:43:33 -0800

    Add another assertion for CiProject#last_build test

--- test/unit/ci_project_test.rb
+++ test/unit/ci_project_test.rb
@@ -3,5 +3,6 @@ require 'test_helper'
 class CiProjectTest < ActiveSupport::TestCase
   test "last_build returns a CiBuild" do
     assert_equal(CiProject.all.first.last_build.class, CiBuild)
+    assert_equal(CiProject.all.last.last_build.class, CiBuild)
   end
 end
