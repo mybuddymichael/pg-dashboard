175b972cda7405656832a2ee520dc026839f55d5
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-10 10:42:04 -0800

    Fix CiProjectTest class name

--- test/unit/ci_project_test.rb
+++ test/unit/ci_project_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CiPojectTest < ActiveSupport::TestCase
+class CiProjectTest < ActiveSupport::TestCase
   test "last_build returns a CiBuild" do
     assert_equal(CiProject.all.first.last_build.class, CiBuild)
   end
