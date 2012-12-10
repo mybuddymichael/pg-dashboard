c3535bc2826d57445a76a0ec2395c960e007803a
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-10 15:50:58 -0800

    Change User to be an ad hoc model, add new auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,39 +1,33 @@
-# == Schema Information
-#
-# Table name: user
-#
-#  userID         :integer          not null, primary key
-#  companyID      :integer          default(1), not null
-#  username       :string(45)       not null
-#  password       :string(40)       not null
-#  firstName      :string(45)       not null
-#  lastName       :string(45)       not null
-#  email          :string(45)       not null
-#  phone          :string(45)       not null
-#  title          :string(255)
-#  role           :string(13)       not null
-#  enabled        :boolean          default(FALSE), not null
-#  verified       :boolean          default(FALSE), not null
-#  emailVerified  :boolean          default(FALSE), not null
-#  licenseVersion :integer          default(0), not null
-#  licenseDate    :date
-#  expirationDate :date
-#
+class User
+  eval(EnvVar::PRIME_API_HASH_FUNCTION)
 
-class User < ActiveRecord::Base
-  if Rails.env == "production" || Rails.env == "development"
-    establish_connection "pdms_accounts"
-    self.table_name = "user"
-  end
+  def self.authenticate(username, password)
+    hash = hash_function(username, password)
 
+    n1 = SecureRandom.random_number(100000) + 1
+    n2 = SecureRandom.random_number(100000) + 1
+    h1 = Digest::SHA1.hexdigest(n1.to_s + hash + n2.to_s + username)
+    h2 = Digest::SHA1.hexdigest(n2.to_s + hash + n1.to_s)
 
-  def authenticate_signin (password)
-    @user = User.find_by_password(
-      Digest::SHA1.hexdigest(password + self.username))
-    if @user && (@user.companyID == 1)
-      return true
-    else
-      return false
-    end
+    request_hash = {
+      headers: {
+        'Content-Type' => 'application/json'
+      },
+      body: ActiveSupport::JSON.encode({
+        messageType: 'ICPMetaDataRequest',
+        messageID: SecureRandom.uuid,
+        'b81f3779-42c8-4aa2-9231-03e7f7284701' => {
+          '75f365b8-2f7d-4574-99c1-8c144a648494' => n1.to_s,
+          'a867fb6e-1e16-4725-a939-e1bbcf987966' => n2.to_s,
+          '988871a4-d095-4879-96ea-f0603497068c' => h1,
+          '8cd8e43a-3d8c-4d38-88ff-2f01bc5d1c18' => h2
+        }
+      })
+    }
+
+    response = HTTParty.post(EnvVar::PRIME_API_HOSTNAME, request_hash)
+    response_hash = ActiveSupport::JSON.decode(response.body)
+
+    response_hash["messageType"] == "InvalidRequest" ? false : true
   end
 end
