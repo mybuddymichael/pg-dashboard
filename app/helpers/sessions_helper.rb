ea1e420b57076a5e0db62107285ef47c4ed5f5ac
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-10 15:51:37 -0800

    Fix a typo

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,7 +29,7 @@ module SessionsHelper
   def signed_in_user
     unless signed_in?
       store_location
-      redirect_to signin_url, notice: "Pleace sign in."
+      redirect_to signin_url, notice: "Please sign in."
     end
   end
 
