3a8a7d20467120945d8f48a6ed3e9fee1cb00f72
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-10 21:02:13 -0800

    Add a new Icp.all and new ad hoc properties for Icp

--- app/models/icp.rb
+++ app/models/icp.rb
@@ -1,7 +1,34 @@
 class Icp
   extend PrimeApi
 
+  def self.all
+    response_hash = get_response
+    response_hash["icps"].collect do |icp|
+      new(name: icp["name"],
+          last_connect_time: icp["lastConnectedToPrime"],
+          last_sync_time: icp["lastSynchronization"],
+          last_parse_time: icp["lastParse"],
+          sync_interval: icp["synchronizationInterval"])
+    end
+  end
 
+  attr_reader :name
+
+  attr_reader :last_connect_time
+
+  attr_reader :last_sync_time
+
+  attr_reader :last_parse_time
+
+  attr_reader :sync_interval
+
+  def initialize(options)
+    @name = options[:name]
+    @last_connect_time = DateTime.parse(options[:last_connect_time])
+    @last_sync_time = DateTime.parse(options[:last_sync_time])
+    @last_parse_time = DateTime.parse(options[:last_parse_time])
+    @sync_interval = options[:sync_interval]
+  end
 
   private
 
