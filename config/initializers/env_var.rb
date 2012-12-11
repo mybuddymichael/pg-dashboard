a8e94cf85b8f2527293a3923ef6d2274cdf1acfd
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-10 21:31:29 -0800

    Remove issue tracker env vars

--- config/initializers/env_var.rb
+++ config/initializers/env_var.rb
@@ -9,10 +9,6 @@ Rails.application.config.before_configuration do
     CI_PASSWORD = ENV["dashboard_ci_password"]
     CI_HOSTNAME = ENV["dashboard_ci_hostname"]
 
-    ISSUE_TRACKER_USERNAME = ENV["dashboard_issue_tracker_username"]
-    ISSUE_TRACKER_PASSWORD = ENV["dashboard_issue_tracker_password"]
-    ISSUE_TRACKER_HOSTNAME = ENV["dashboard_issue_tracker_hostname"]
-
     self.constants.each do |constant|
       if EnvVar.const_get(constant).nil?
         raise TypeError, "EnvVar::#{constant} is nil! Define the relevant environment variable first. (See `config/initializers/env_var.rb` for necessary env vars.)"
