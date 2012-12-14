7a16832a7c1245d57c0fceb15c5da2c5b2ef6427
Aris Turner <bootstraps3412@gmail.com>
2012-12-13 16:08:30 -0800

    Update git parser to ignore old data, add hook script

--- app/controllers/git_logs_controller.rb
+++ app/controllers/git_logs_controller.rb
@@ -1,16 +1,18 @@
 class GitLogsController < ApplicationController
-  
-  def new
-    @git_commit = GitLog.new(
-      :sha => params["commit"],
-      :message => params["message"],
-      :author => params["author"],
-      :email => params["email"],
-      :commit_time => DateTime.parse(params["date"]),
-      :branch => params["branch"]
-    )
 
-    @git_commit.save
+  def new
+    if !GitLog.find_by_sha_and_branch(params["commit"],params["branch"])
+      @git_commit = GitLog.new(
+        :sha => params["commit"],
+        :message => params["message"],
+        :author => params["author"],
+        :email => params["email"],
+        :commit_time => DateTime.parse(params["date"]),
+        :branch => params["branch"]
+      )
+      @git_commit.save
+    end
+
     render :nothing => true
   end
 end
--- gitlogs.sh
+++ gitlogs.sh
@@ -1,4 +1,5 @@
-# temp=` git log grails-casperjs --pretty=format:'"%h": {%n  "commit": "%H",%n  "author": {"name": "%an, "email: "<%ae>"},%n  "date": "%ad,%n  "message": "%s"%n}' --max-count=10`
-# curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "${temp}" http://requestb.in/vu1jnbvu
-
-
+#!/bin/sh
+for branch in $(git for-each-ref --format='%(refname:short)' refs/heads/); do                                                                                                   
+  temp=`git log $branch --pretty=format:"{\"branch\":\"${branch}\",\"commit\":\"%H\",\"author\":\"%an\",\"email\":\"%ae\",\"date\":\"%ad\",\"message\":\"%s\"}" --max-count=1`
+  curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "${temp}" http://127.0.0.1:3000/update_git
+done
