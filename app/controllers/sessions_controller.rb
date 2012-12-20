50955576b9110234de5ca618ff806c877e33529b
Aris Turner <bootstraps3412@gmail.com>
2012-12-20 10:21:12 -0800

    update css to account for other mobile devices

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       sign_in(user)
       redirect_to "/dashboard"
     else
-      flash[:error] = "Invalid email/password combination!"
+      flash[:error] = "Invalid username/password combination!"
       render 'new'
     end
   end
